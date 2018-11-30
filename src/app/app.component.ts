f1c1ce3c641db8d2f1be31bccf4ec06d259cb0ad
Marcus Hellberg <marcus@vaadin.com>
2018-11-30 11:01:23 -0600

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-grid';
+import '@vaadin/vaadin-text-field';
+import { FormGroup, FormControl } from '@angular/forms';
 
+class Person {
+  constructor(public firstName: string, public lastName: string) {}
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'how-to-use-web-components-in-angular';
+  form = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl('')
+  });
+
+  people: Person[] = [];
+
+  addPerson() {
+    this.people = [
+      ...this.people,
+      new Person(this.form.value.firstName, this.form.value.lastName)
+    ];
+    this.form.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
